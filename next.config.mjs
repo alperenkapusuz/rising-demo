4832fc26637df3a3c2cc7a422b8adbcb05a896b9
alperen kapusuz <alperen.kapusuz@clockwork.com.tr>
2024-05-30 18:31:26 +0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import dotenv from 'dotenv';
+dotenv.config({
+  path: `./.env.${process.env.NODE_ENV}`,
+});
+
+const nextConfig = {
+    env: {
+        API_URL: process.env.API_URL,
+        PUBLIC_URL: process.env.PUBLIC_URL,
+    },
+    images: {
+      domains: [process.env.PUBLIC_URL],
+      unoptimized: true,
+    },
+    output: "standalone",
+    trailingSlash: true,
+    poweredByHeader: false
+};
 
 export default nextConfig;
